0691ab5cc8158a2935ccabd863f422d7b5ce021c
Frank Natividad <contact@franknatividad.com>
2013-09-09 01:56:22 -0600

    Adding comments to describe each portion.

--- hw1.pl
+++ hw1.pl
@@ -1,4 +1,4 @@
-
+%Intersection: check two lists for entries which exist in both.
 inter([],[],X):- append([],[],X).
 inter([],List,X):- inter([],[],X).
 inter([Element|Rem],List,X):- (member(Element,List)),
@@ -6,6 +6,7 @@ inter([Element|Rem],List,X):- (member(Element,List)),
 							  inter(Rem,List,X1).
 inter([_|Rem],List,X):- inter(Rem,List,X).
 
+%Union: combine two lists into one
 union([],X,X).
 union([],[],X).
 union([Element|Rem],List,X):- (\+member(Element,List)),
@@ -13,6 +14,7 @@ union([Element|Rem],List,X):- (\+member(Element,List)),
 							  union(Rem,X1,X).
 union([_|Rem],List,X):- union(Rem,List,X).
 
+%Count all zero elements in list
 countZeros([],N):- N is 0.
 countZeros([Head|Rem],N):- Head is 0, 
 						   countZeros(Rem,N1), 
@@ -20,6 +22,7 @@ countZeros([Head|Rem],N):- Head is 0,
 countZeros([_|Rem],N):- countZeros(Rem,N1), 
 						N is N1.
 
+%Sum all numbers in list.
 sumAll([],S):- S is 0.
 sumAll([Head|Rem],S):- sumAll(Rem,S1), 
 					   S is Head + S1.
